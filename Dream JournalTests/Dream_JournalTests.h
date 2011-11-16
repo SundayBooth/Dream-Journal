722279adb1b94e22ff04ace8a8f7ab4982ed30a9
Bryan M Sunday-Booth <bryan.sunday-booth@tbr.edu>
2011-11-16 15:43:54 -0600

    first commit

---
+++ Dream JournalTests/Dream_JournalTests.h
@@ -0,0 +1,13 @@
+//
+//  Dream_JournalTests.h
+//  Dream JournalTests
+//
+//  Created by Bryan Sunday-Booth on 10/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Dream_JournalTests : SenTestCase
+
+@end
---
+++ Dream JournalTests/Dream_JournalTests.m
@@ -0,0 +1,32 @@
+//
+//  Dream_JournalTests.m
+//  Dream JournalTests
+//
+//  Created by Bryan Sunday-Booth on 10/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Dream_JournalTests.h"
+
+@implementation Dream_JournalTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Dream JournalTests");
+}
+
+@end
